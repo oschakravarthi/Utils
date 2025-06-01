4ee974bf67c7c9d0f84726b99073be1b4f71198d
Subrahmanya Chakravarthi Oruganti <msarchitect@rocketmail.com>
2025-06-01 23:16:16 +0530

    updated packages

--- SubhadraSolutions.Utils.Blazor/Components/ExtendedAutocomplete.cs
+++ SubhadraSolutions.Utils.Blazor/Components/ExtendedAutocomplete.cs
@@ -1,22 +1,22 @@
-using MudBlazor;
-using SubhadraSolutions.Utils.Blazor.Extensions;
-using System.Reflection;
+//using MudBlazor;
+//using SubhadraSolutions.Utils.Blazor.Extensions;
+//using System.Reflection;
 
-namespace SubhadraSolutions.Utils.Blazor.Components;
+//namespace SubhadraSolutions.Utils.Blazor.Components;
 
-public class ExtendedAutocomplete<T> : MudAutocomplete<T>
-{
-    private void FixAccessibilityCore()
-    {
-        var _elementReferencetField = GetType().BaseType
-            .GetField("_elementReference", BindingFlags.Instance | BindingFlags.NonPublic);
+//public class ExtendedAutocomplete<T> : MudAutocomplete<T>
+//{
+//    private void FixAccessibilityCore()
+//    {
+//        var _elementReferencetField = GetType().BaseType
+//            .GetField("_elementReference", BindingFlags.Instance | BindingFlags.NonPublic);
 
-        var inputElement = (MudInput<T>)_elementReferencetField.GetValue(this);
-        if (inputElement != null)
-        {
-            var elementReference = inputElement.ElementReference;
-            elementReference.SetAttribute("autocomplete", "off");
-            elementReference.SetAttribute("aria-label", AdornmentAriaLabel);
-        }
-    }
-}
+//        var inputElement = (MudInput<T>)_elementReferencetField.GetValue(this);
+//        if (inputElement != null)
+//        {
+//            var elementReference = inputElement.ElementReference;
+//            elementReference.SetAttribute("autocomplete", "off");
+//            elementReference.SetAttribute("aria-label", AdornmentAriaLabel);
+//        }
+//    }
+//}
