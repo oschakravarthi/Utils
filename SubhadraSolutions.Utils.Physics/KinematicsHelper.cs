1eb6ee4e412fb601b394f47a66b019da8f89af75
Subrahmanya Chakravarthi Oruganti <msarchitect@rocketmail.com>
2025-06-28 10:53:54 +0530

    improvements

---
+++ SubhadraSolutions.Utils.Physics/KinematicsHelper.cs
@@ -0,0 +1,12 @@
+﻿namespace SubhadraSolutions.Utils.Physics
+{
+    public static class KinematicsHelper
+    {
+        public static double CalculateDisplacement(double u, double v, double t)
+        {
+            var a = v - u;
+            var s = (u * t) + ((a * t * t) / 2);
+            return s;
+        }
+    }
+}
