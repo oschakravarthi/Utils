99782c17e47900c1cedbd058a0d2c265a351058d
Subrahmanya Chakravarthi Oruganti <msarchitect@rocketmail.com>
2025-06-28 11:25:57 +0530

    improvements

--- SubhadraSolutions.Utils.Physics/KinematicsHelper.cs
+++ SubhadraSolutions.Utils.Physics/KinematicsHelper.cs
@@ -8,5 +8,11 @@
             var s = (u * t) + ((a * t * t) / 2);
             return s;
         }
+        public static double CalculateTime(double u, double v, double remainingDisplacement)
+        {
+            var a = v - u;
+            var t = Math.Sqrt(2 * remainingDisplacement / a);
+            return t;
+        }
     }
 }
