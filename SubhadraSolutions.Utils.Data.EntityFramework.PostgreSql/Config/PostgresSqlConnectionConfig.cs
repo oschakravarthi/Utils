e1c4380f186f60e2aa11fbea0678ba732b61d3d6
Subrahmanya Chakravarthi Oruganti <msarchitect@rocketmail.com>
2025-09-08 03:56:45 +0530

    improvements

---
+++ SubhadraSolutions.Utils.Data.EntityFramework.PostgreSql/Config/PostgresSqlConnectionConfig.cs
@@ -0,0 +1,12 @@
+﻿namespace SubhadraSolutions.Utils.Data.EntityFramework.PostgreSql.Config
+{
+    public class PostgresSqlConnectionConfig
+    {
+        public string Host { get; set; }
+        public int Port { get; set; }
+        public string DatabaseName { get; set; }
+        public string SchemaName { get; set; }
+        public string UserName { get; set; }
+        public string Password { get; set; }
+    }
+}
