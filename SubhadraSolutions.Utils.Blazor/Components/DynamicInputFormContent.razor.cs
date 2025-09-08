040f19dd6faa92ce4e239626c33395ce189e43e2
Subrahmanya Chakravarthi Oruganti <msarchitect@rocketmail.com>
2025-09-08 21:54:27 +0530

    improvements

--- SubhadraSolutions.Utils.Blazor/Components/DynamicInputFormContent.razor.cs
+++ SubhadraSolutions.Utils.Blazor/Components/DynamicInputFormContent.razor.cs
@@ -3,14 +3,15 @@ using MudBlazor;
 using SubhadraSolutions.Utils.Data;
 using System.Collections.Generic;
 using System.Threading.Tasks;
+using Newtonsoft.Json.Schema;
 
 namespace SubhadraSolutions.Utils.Blazor.Components
 {
     public partial class DynamicInputFormContent
     {
         [Parameter]
-        public FieldSchema Schema { get; set; }
-
+        public JSchema Schema { get; set; }
+       
         [Parameter]
         public Dictionary<string, object> Model { get; set; }
     }
--- SubhadraSolutions.Utils.Core/Data/FieldSchema.cs
+++ SubhadraSolutions.Utils.Core/Data/FieldSchema.cs
@@ -1,43 +1,43 @@
-﻿using Newtonsoft.Json;
-using Newtonsoft.Json.Serialization;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using Newtonsoft.Json;
+//using Newtonsoft.Json.Serialization;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace SubhadraSolutions.Utils.Data
-{
-    public class FieldSchema
-    {
-        [JsonProperty("name")]
-        public string Name { get; set; }
+//namespace SubhadraSolutions.Utils.Data
+//{
+//    public class FieldSchema
+//    {
+//        [JsonProperty("name")]
+//        public string Name { get; set; }
 
-        [JsonProperty("label")]
-        public string Label { get; set; }
+//        [JsonProperty("label")]
+//        public string Label { get; set; }
 
-        [JsonProperty("type")]
-        public string Type { get; set; }   // string, integer, object, array, etc.
+//        [JsonProperty("type")]
+//        public string Type { get; set; }   // string, integer, object, array, etc.
 
 
-        [JsonProperty("required")]
-        public bool Required { get; set; }
+//        [JsonProperty("required")]
+//        public bool Required { get; set; }
 
-        [JsonProperty("placeholder")]
-        public string Placeholder { get; set; }
+//        [JsonProperty("placeholder")]
+//        public string Placeholder { get; set; }
 
-        [JsonProperty("description")]
-        public string Description { get; set; }
+//        [JsonProperty("description")]
+//        public string Description { get; set; }
 
-        [JsonProperty("pattern")]
-        public string Pattern { get; set; }
+//        [JsonProperty("pattern")]
+//        public string Pattern { get; set; }
 
-        // For nested structures
+//        // For nested structures
 
-        [JsonProperty("fields")]
-        public List<FieldSchema> Fields { get; set; }  // object
+//        [JsonProperty("fields")]
+//        public List<FieldSchema> Fields { get; set; }  // object
 
-        [JsonProperty("items")]
-        public FieldSchema Items { get; set; }         // array
-    }
-}
+//        [JsonProperty("items")]
+//        public FieldSchema Items { get; set; }         // array
+//    }
+//}
